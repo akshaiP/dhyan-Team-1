76f276218a885ff790c3db53bc7dfa092bfe0beb
akshaiP <158182355+akshaiP@users.noreply.github.com>
2024-09-11 11:46:12 +0530

    Filter bugs fixed

--- Frontend-careersite/src/app/pipe/filter.pipe.ts
+++ Frontend-careersite/src/app/pipe/filter.pipe.ts
@@ -12,11 +12,10 @@ export class FilterPipe implements PipeTransform {
   
     return jobList.filter(job => {
     
-  
-      const matchesExperience = experience === 'all' || job.experienceLevel.toLowerCase() === experience.toLowerCase();
-      const matchesLocation = location === 'all' || job.location.toLowerCase() === location.toLowerCase();
-      const matchesJobType = jobType === 'all' || job.employmentType.toLowerCase() === jobType.toLowerCase();
-      const matchesSalaryRange = salaryRange === 'all' || job.salaryRange.toLowerCase() === salaryRange.toLowerCase();
+      const matchesExperience = experience === 'all' || (job.experienceLevel && job.experienceLevel.toLowerCase() === experience.toLowerCase());
+      const matchesLocation = location === 'all' || (job.location && job.location.toLowerCase() === location.toLowerCase());
+      const matchesJobType = jobType === 'all' || (job.employmentType && job.employmentType.toLowerCase() === jobType.toLowerCase());
+      const matchesSalaryRange = salaryRange === 'all' || (job.salaryRange && job.salaryRange.toLowerCase() === salaryRange.toLowerCase());
   
       return matchesLocation && matchesExperience && matchesJobType && matchesSalaryRange;
     });
